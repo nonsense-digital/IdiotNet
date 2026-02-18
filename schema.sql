0af2d3b75a6ea72f3b7e825aa0162ec8e326ddda
W1THRD <W1THRD@outlook.com>
2026-02-17 17:50:30 -0700

    Bugfix and schema comments

--- schema.sql
+++ schema.sql
@@ -1,20 +1,33 @@
+-- CUSTOM TEXT SEARCH DICTIONARY
+-- Similar to the default dictionary in Postgres, except it includes ALL english words
+-- this means common articles such as 'a' or 'the' will be included
 -- Source - https://stackoverflow.com/questions/1497895/can-i-configure-postgresql-programmatically-to-not-eliminate-stop-words-in-full
 -- Posted by catchdave, modified by community. See post 'Timeline' for change history
 -- Retrieved 2026-01-10, License - CC BY-SA 4.0
-
 CREATE TEXT SEARCH DICTIONARY english_stem_nostop (
     Template = snowball
     , Language = english
 );
-
 CREATE TEXT SEARCH CONFIGURATION public.english_nostop ( COPY = pg_catalog.english );
 ALTER TEXT SEARCH CONFIGURATION public.english_nostop
    ALTER MAPPING FOR asciiword, asciihword, hword_asciipart, hword, hword_part, word WITH english_stem_nostop;
 
+-- ROLES
+-- Member = Standard, common user that can post, comment, like, etc.
+-- Moderator = Can give punishments, censor content, and view IP addresses
+-- Admin = Can change user roles and modify server settings
 create type role as enum ('member', 'moderator', 'admin');
 
+-- PUNISHMENT TYPES
+-- None = normal, no restriction
+-- Ban = user/IP cannot access the website
+-- Mute = user/IP can view the website, but cannot add or edit any content
+-- Permaban = user is permanently removed from the website and all data is erased
 create type punishmenttype as enum ('none', 'mute', 'ban', 'permaban');
 
+-- COMMENTS
+-- Text-only, single-line blurbs that users can add to any post
+-- Comments can also be replies to other comments, creating "threads" of comments
 create table comments
 (
     id           integer generated always as identity (minvalue 0)
@@ -28,6 +41,8 @@ create table comments
     comment_page integer                       not null
 );
 
+-- IMAGES
+-- Image files uploading by users, tied to posts
 create table images
 (
     id            integer generated always as identity (minvalue 0)
@@ -38,6 +53,8 @@ create table images
     post          integer not null
 );
 
+-- POSTS
+-- These are the main pieces of user-generated content on the site. They are tied to users.
 create table posts
 (
     id            integer generated always as identity (minvalue 0),
@@ -52,9 +69,14 @@ create table posts
     approved      boolean default true
 );
 
+-- POST INDEXING
+-- Indexes the words used so they can be searched
 create index idx_posts_ts_nostop
     on posts using gin (ts_nostop);
 
+-- AUTH TOKENS
+-- These are stored in cookies, allowing authentication to be remembered on a web browser
+-- They expire after 7 days of inactivity
 create table tokens
 (
     id          uuid      not null,
@@ -63,6 +85,9 @@ create table tokens
     client      inet      not null
 );
 
+-- USERS
+-- Represents user accounts and their profiles
+-- Can be tracked and punished by moderators/admins
 create table users
 (
     id                    integer generated always as identity (minvalue 0),
@@ -85,6 +110,8 @@ create table users
 INSERT INTO users (username, email, date_created, password_hash, role) VALUES
              ('admin', null, now(), '$2b$12$kqWB4xcHxygsZhFmiAgYc.rWsy1pRvZ5OJFGk837exLsHRvF0SfvO', 'admin');
 
+-- CONFIGURATION
+-- Key-value settings for various server-wide functions
 create table config
 (
     key   varchar(20) not null
@@ -96,13 +123,18 @@ create table config
 -- CONFIGURATION VALUES
 -- The "version" config value is used to determine the version of the database
 -- This is done to prevent version mismatch between the IdiotNet backend and the database schema
+-- Other configuration settings are used for website security and moderation
 INSERT INTO config VALUES ('version', '1.0');
-INSERT INTO config VALUES ('join-code', null);
+INSERT INTO config VALUES ('join_code', null);
 INSERT INTO config VALUES ('allow_signup', true);
 INSERT INTO config VALUES ('approve_posts', false);
 INSERT INTO config VALUES ('support_email', null);
 INSERT INTO config VALUES ('announcement_banner', null);
+INSERT INTO config VALUES ('require_email', false);
 
+-- CLIENTS
+-- Keeps track of previously connected IP addresses
+-- Used for website security and moderation
 create table clients
 (
     ip_address            inet not null
@@ -111,9 +143,11 @@ create table clients
     last_accessed         timestamp,
     punishment_status     punishmenttype default 'none'::punishmenttype,
     punishment_expiration timestamp      default now(),
-    punishment_reason     varchar(50)    default ''::character varying,
+    punishment_reason     varchar(50)    default ''::character varying
 );
 
+-- FOLLOWS
+-- Keeps track of follower relationships between users
 create table follows
 (
     id            integer generated always as identity (minvalue 0)
@@ -124,6 +158,8 @@ create table follows
     date_followed timestamp
 );
 
+-- LIKES
+-- Keeps track of like relationships between posts and users
 create table likes
 (
     id         integer generated always as identity (minvalue 0)
@@ -134,3 +170,14 @@ create table likes
     date_liked timestamp
 );
 
+-- VERIFICATION CHALLENGES
+-- A token used for email verification
+create table verify
+(
+    id          uuid        not null
+        constraint verify_pk
+            primary key,
+    user_id     integer     not null,
+    valid_until timestamp   not null,
+    email       varchar(60) not null
+);
